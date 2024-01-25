12dc1466b03180940cee2f2108291eedf483c043
Daniel Saint-Geours <danisg96@hotmail.it>
2024-01-25 20:53:07 +0100

    Creato test per BookmarController

---
+++ src/test/java/com/segnalibri/api/Segnalibri/controller/BookmarkControllerTest.java
@@ -0,0 +1,200 @@
+package com.segnalibri.api.Segnalibri.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.segnalibri.api.Segnalibri.model.Bookmark;
+import com.segnalibri.api.Segnalibri.repository.BookmarkRepository;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest(controllers = BookmarkController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class BookmarkControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    BookmarkRepository bookmarkRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private Bookmark bookmark;
+
+    @BeforeEach
+    void setUp() {
+        bookmark = Bookmark.builder()
+                .title("Sito1")
+                .description("Primo segnalibro")
+                .url("https://primosito.com")
+                .userId(1)
+                .build();
+    }
+
+    @Test
+    @Order(1)
+    void userIsCreatedTest() throws Exception {
+        given(bookmarkRepository.save(ArgumentMatchers.any(Bookmark.class)))
+                .willAnswer(invocation -> invocation.getArgument(0));
+
+        ResultActions response = mockMvc.perform(post("/api/1/bookmark")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(bookmark)));
+
+        response.andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    @Order(2)
+    void shouldGetAllBookmark() throws Exception {
+        when(bookmarkRepository.findAllByUserId(ArgumentMatchers.anyInt()))
+                .thenReturn(List.of(bookmark));
+
+        ResultActions response = mockMvc.perform(get("/api/1/bookmark")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].title", CoreMatchers.is("Sito1")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].url", CoreMatchers.is("https://primosito.com")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].description", CoreMatchers.is("Primo segnalibro")));
+    }
+
+    @Test
+    @Order(3)
+    void shouldFindById() throws Exception {
+        given(bookmarkRepository.findById(ArgumentMatchers.anyInt()))
+                .willAnswer(invocation -> Optional.of(Bookmark.builder()
+                                .id(invocation.getArgument(0))
+                                .title("Sito1")
+                                .description("Primo segnalibro")
+                                .url("https://primosito.com")
+                                .userId(1)
+                                .build())
+                );
+        ResultActions response = mockMvc.perform(get("/api/1/bookmark/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", CoreMatchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title", CoreMatchers.is("Sito1")));
+    }
+
+    @Test
+    @Order(4)
+    void shouldNotFindById() throws Exception {
+        when(bookmarkRepository.findById(ArgumentMatchers.anyInt()))
+                .thenReturn(Optional.empty());
+
+        ResultActions response = mockMvc.perform(get("/api/1/bookmark/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @Order(5)
+    void shouldUpdateGivenId() throws Exception {
+        given(bookmarkRepository.findById(ArgumentMatchers.anyInt()))
+                .willAnswer(invocation -> Optional.of(Bookmark.builder()
+                        .id(invocation.getArgument(0))
+                        .title("Sito1")
+                        .description("Primo segnalibro")
+                        .url("https://primosito.com")
+                        .userId(1)
+                        .build())
+                );
+
+        given(bookmarkRepository.save(ArgumentMatchers.any(Bookmark.class)))
+                .willAnswer(invocation -> invocation.getArgument(0));
+
+        ResultActions response = mockMvc.perform(put("/api/1/bookmark/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(bookmark)));
+
+        response.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    @Order(6)
+    void shouldNotFindIdAndNotPostTest() throws Exception {
+
+        when(bookmarkRepository.findById(ArgumentMatchers.anyInt()))
+                .thenReturn(Optional.empty());
+
+        ResultActions response = mockMvc.perform(put("/api/1/bookmark/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(bookmark)));
+
+        response.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @Order(7)
+    void shouldDeleteById() throws Exception {
+        doNothing().when(bookmarkRepository).deleteById(ArgumentMatchers.anyInt());
+
+        ResultActions response = mockMvc.perform(delete("/api/1/bookmark/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    @Order(8)
+    void shouldFindByTitle() throws Exception {
+        Page<Bookmark> page = Mockito.mock(Page.class);
+        when(page.getTotalElements()).thenReturn(1L);
+
+        when(bookmarkRepository.findAllByUserIdAndTitleContaining(ArgumentMatchers.anyInt(),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.any())).thenReturn(page);
+
+        ResultActions response = mockMvc.perform(get("/api/1/bookmark/title/i/0/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @Order(9)
+    void ShouldNotFindByTitle() throws Exception{
+        Page<Bookmark> page = Mockito.mock(Page.class);
+        when(page.getTotalElements()).thenReturn(0L);
+
+        when(bookmarkRepository.findAllByUserIdAndTitleContaining(ArgumentMatchers.anyInt(),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.any())).thenReturn(page);
+
+        ResultActions response = mockMvc.perform(get("/api/1/bookmark/title/i/0/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isNotFound());
+
+    }
+}
+
